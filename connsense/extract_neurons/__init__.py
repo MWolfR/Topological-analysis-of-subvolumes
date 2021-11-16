a643ca64c060741f6dbb119821d8a6834e7d9d65
Michael Reimann <michael@reimann.science>
2021-11-16 16:49:19 +0100

    This should extract neurons depths, if specified in the configuration

--- connsense/extract_neurons/__init__.py
+++ connsense/extract_neurons/__init__.py
@@ -2,6 +2,7 @@
 from collections.abc import Mapping
 
 import pandas as pd
+import numpy
 
 from bluepy import Circuit
 
@@ -11,6 +12,20 @@ from ..io import logging
 
 STEP = "extract-neurons"
 LOG = logging.get_logger(STEP)
+NEURON_XYZ = ["x", "y", "z"]
+
+
+def get_neuron_depths(circuit):
+    LOG.info("RUN neuron depths extraction")
+    from flatmap_utility import supersampled_neuron_locations
+    #  TODO: Use config-provided flatmap, if possible
+    #  TODO: Could offer diffent ways to get the depths values here, such as, using [PH]y
+    orient = circuit.atlas.load_data("orientation")
+    flatmap = circuit.atlas.load_data("flatmap")
+    flat_and_depths = supersampled_neuron_locations(circuit, flatmap, orient, include_depth=True)
+    depths = flat_and_depths[["depth"]]
+    LOG.info("DONE neuron depths extractions")
+    return depths
 
 
 def extract(circuits, subtargets, params):
@@ -22,11 +37,23 @@ def extract(circuits, subtargets, params):
 
     circuits = dict([(k, Circuit(v)) for k, v in circuits.items()])
 
+    # TODO: find a better way
+    if "depth" in params:
+        depths = dict([(k, get_neuron_depths(v)) for k, v in circuits.items()])
+        params.remove("depth")
+        include_depth = True
+    else:
+        include_depth = False
+
     #circuit_frame = subtargets.index.to_frame().apply(lambda x: circuits[x["circuit"]], axis=1)
 
     def get_props(index, gids):
         circuit = circuits[index[0]]
         props = circuit.cells.get(gids, properties=params)
+        if include_depth:
+            circ_depth = depths[index[0]]
+            nrn_depths = circ_depth.loc[circ_depth.index.intersection(gids)]
+            props = pd.concat(props, nrn_depths, axis=1)  # Should fill missing gids with NaN
         props.index = pd.MultiIndex.from_tuples([index + (gid,) for gid in gids],
                                                 names=["circuit", "subtarget",
                                                        "flat_x", "flat_y", "gid"])
